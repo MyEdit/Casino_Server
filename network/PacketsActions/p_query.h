aedd4f498d9769728cc45fcf02161f254114c87d
Vsevolod <rusakevich.ceva@yandex.ru>
2024-02-29 20:43:55 +0300

    убраны заготовки запросов

--- network/PacketsActions/p_query.h
+++ network/PacketsActions/p_query.h
@@ -8,20 +8,9 @@
 
 class P_Query
 {
-    static QMap<QueryTypes, QString> querys;
-
 private:
     static void getResultQuary(QSharedPointer<SOCKET> clientSocket);
     static void sendResult(QSharedPointer<SOCKET> clientSocket, QString result, ModelTypes modelTypes, QueryTypes queryTypes);
-    static void initMapQuerys();
-    static QString distributor(QueryTypes queryTypes, ModelTypes modelTypes, QSharedPointer<SOCKET> clientSocket);
-
-    static QString countEntrites(QueryTypes queryTypes, ModelTypes modelTypes, QSharedPointer<SOCKET> clientSocket);
-    static QString numberRow(QueryTypes queryTypes, ModelTypes modelTypes, QSharedPointer<SOCKET> clientSocket);
-    static QString createEntry(QueryTypes queryTypes, ModelTypes modelTypes, QSharedPointer<SOCKET> clientSocket);
-    static QString updateEntry(QueryTypes queryTypes, ModelTypes modelTypes, QSharedPointer<SOCKET> clientSocket);
-    static QString deleteEntry(QueryTypes queryTypes, ModelTypes modelTypes, QSharedPointer<SOCKET> clientSocket);
-    static QString search(QueryTypes queryTypes, ModelTypes modelTypes, QSharedPointer<SOCKET> clientSocket);
 
 public:
     friend class NetworkServer;
