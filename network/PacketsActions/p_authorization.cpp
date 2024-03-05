aadaea07b43b15620034742013402276f791c0c8
Vsevolod <rusakevich.ceva@yandex.ru>
2024-03-05 22:20:31 +0300

    Добавил отправку ФИО юзера при авторизации

--- network/PacketsActions/p_authorization.cpp
+++ network/PacketsActions/p_authorization.cpp
@@ -8,23 +8,32 @@ void P_Authorization::authorizeClient(QSharedPointer<SOCKET> clientSocket)
     QString password = NetworkServer::getMessageFromClient(clientSocket);
     QString query = QString("SELECT ID_Role FROM StuffUsers WHERE Login = '%1' AND Password = '%2'").arg(login).arg(password);
     QString responce = databaseManager->executeQuery(query);
+    QString fio;
 
-    if (responce == nullptr)
+    if (responce.isEmpty())
     {
         query = QString("SELECT ID_Role FROM Users WHERE Login = '%1' AND Password = '%2'").arg(login).arg(password);
         responce = databaseManager->executeQuery(query);
 
-        if (responce == nullptr)
+        if (responce.isEmpty())
         {
             P_Notification::sendNotification(clientSocket, TypeMessage::Error, "Uncorrect login or password");
             return;
         }
+
+        query = QString("SELECT Name FROM Users WHERE Login = '%1' AND Password = '%2'").arg(login).arg(password);
+        fio = databaseManager->executeQuery(query);
     }
+
+    query = QString("SELECT Name FROM StuffUsers WHERE Login = '%1' AND Password = '%2'").arg(login).arg(password);
+    fio = databaseManager->executeQuery(query);
 
     Message::logInfo("User " + login + " successfully logged");
     PacketTypes packettype = PacketTypes::P_Authorization;
     Roles role = static_cast<Roles>(responce.toInt());
+
     NetworkServer::sendToClient(clientSocket, &packettype, sizeof(PacketTypes));
     NetworkServer::sendToClient(clientSocket, &role, sizeof(Roles));
+    NetworkServer::sendToClient(clientSocket, fio);
     NetworkServer::addConnect(clientSocket, login);
 }
