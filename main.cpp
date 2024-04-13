6cb27a3a99cbfcd0f62851508c8b92c75d38ec62
Всеволод Русакевич <rusakevich.ceva@yandex.ru>
2024-04-13 15:07:28 +0300

    Бакфикс чтобы резил потом нормально работал

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
-﻿#include <Network/networkserver.h>
+﻿#include <QCoreApplication>
+#include <Network/networkserver.h>
 #include <Commands/commandmanager.h>
 
 NetworkServer network;
 CommandManager commandManager;
 
-int main()
+int main(int argc, char *argv[])
 {
+    QCoreApplication app(argc, argv);
+
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
     network.init();
     commandManager.init();
     network.startListening();
-    return 0;
+
+    return app.exec();
 }
