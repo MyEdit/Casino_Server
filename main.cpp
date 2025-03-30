55491c78d7a225d75d3462d2c27c7b5995409c17
Всеволод Русакевич <rusakevich.ceva@yandex.ru>
2025-03-30 20:15:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,12 +9,33 @@ NetworkServer network;
 CommandManager commandManager;
 Updater updater;
 
+void initIP()
+{
+    QString filename = "conf.txt";
+    QFile file(filename);
+
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        Message::logError("отсутстувует conf.txt");
+        exit(-1);
+    }
+
+    QTextStream in(&file);
+    while(!in.atEnd())
+    {
+        network.ADDRESS = in.readLine();
+        return;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
+    initIP();
 
     Ticker::init();
     network.init();
